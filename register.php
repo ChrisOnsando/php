2f977d9b78b1f30e7fd52f7b56f9d31335dbf861
Chris Onsando <chrisnemwel@gmail.com>
2022-02-10 14:55:36 +0300

    first commit

---
+++ register.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Register</title>
+<!--Google font-->
+
+<link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- font awesome icons -->
+
+    <link
+      rel="stylesheet"
+      href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:wght@400;600;700;800;900&display=swap"
+      rel="stylesheet"
+    />
+
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+      integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+      crossorigin="anonymous"
+    ></script>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+      integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+      crossorigin="anonymous"
+    ></script>
+<body class="bg-primary">
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                <div class="col-lg-5">
+                image Here
+ </div>
+                <div class="col-lg-7">
+                <div class="text-center">
+                    <h1 class="h4 grey mb-4">Create Account</h1>
+<form action="">
+
+<div class="row m-3">
+
+<div class="col-sm-6 m-3">
+<input class="form-control rounded-pill" type="text" name="firstname" placeholder="firstname">
+</div>
+
+<div class="col-sm-6 m-3">
+<input class="form-control rounded-pill" type="text" name="secondname" placeholder="secondname">
+</div>
+
+<div class="row m-3">
+<input class="form-control rounded-pill" type="email" name="emailaddress" placeholder="emailaddress">
+</div>
+
+<div class="row m-3">
+
+<div class="col-sm-6 m-3">
+<input class="form-control rounded-pill" type="password" name="password" placeholder="password">
+</div>
+
+<div class="col-sm-6">
+<input class="form-control rounded-pill" type="password" name="Confirmpassword" placeholder="Confirmpassword">
+</div>
+</div>
+
+<div class="row m-3">
+    <input class="form-control rounded-pill" type="submit" class="btn btn-primary" name="Register">
+</div>
+
+<hr>
+
+<div>
+    <a href="" class="btn btn-outline-danger">Register with Google?</a>
+</div>
+
+<div>
+    <a href="" class="small text-center">Forgot Password?</a>
+</div>
+
+<div>
+    <a href="" class="text-primary text-center">Already Have an Account?</a>
+</div>
+
+</form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
